f828c8bf041295e2d527f7a7107a26bbd3070534
rameznoori <199920071+rameznoori@users.noreply.github.com>
2025-09-14 03:36:57 +0530

    configured the converstaion with agent's first message and system prompt

--- virtual_assistant.py
+++ virtual_assistant.py
@@ -11,7 +11,31 @@ AGENT_ID = os.getenv("AGENT_ID")
 API_KEY = os.getenv("API_KEY")
 
 user_name = "Ramez"
-schedule = "Project meeting with Shehnaz at 13:00; Interview with Microsoft at 16:00"
+schedule = "Project meeting with Andre at 13:00; Interview with Microsoft at 16:00"
 prompt = f"Your conversationalist has the following schedule: {schedule}."
 first_message = f"Hello {user_name}, how can I help you today?"
 
+conversation_override = {
+    "agent": {
+        "prompt": {
+            "prompt": prompt,
+        },
+        "first_message": first_message,
+    },
+}
+
+config = ConversationConfig(
+    conversation_config_override = conversation_override,
+    extra_body = {},
+    dynamic_variables = {},
+)
+
+client = ElevenLabs(api_key=API_KEY)
+
+conversation = Conversation(
+    client,
+    AGENT_ID,
+    config=config,
+    requires_auth=True,
+    audio_interface=DefaultAudioInterface(),
+)
